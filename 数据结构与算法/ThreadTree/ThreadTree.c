49eb39a89e0180cf3103e3b361d365b73e4d06c1
pompey1 <2296593273@qq.com>
2023-03-28 23:19:08 +0800

    123

--- 数据结构与算法/ThreadTree/ThreadTree.c
+++ 数据结构与算法/ThreadTree/ThreadTree.c
@@ -18,7 +18,19 @@ typedef struct TreeNode
     int rTag;
 } TreeNode;
 
+typedef struct TripleTreeNode
+{
+    char data;
+    int flag;
+    struct TripleTreeNode *lchild;
+    struct TripleTreeNode *rchild;
+    struct TripleTreeNode *parent;
+    int lTag;
+    int rTag;
+} TripleTreeNode;
+
 typedef TreeNode *Tree;
+typedef TripleTreeNode *TripleTree;
 
 Bool TryCreateTree(Tree *tree, char *string, int *index)
 {
@@ -43,6 +55,34 @@ Bool TryCreateTree(Tree *tree, char *string, int *index)
         (*tree)->flag = 0;
         TryCreateTree(&(*tree)->lchild, string, index);
         TryCreateTree(&(*tree)->rchild, string, index);
+    }
+    return true;
+}
+
+Bool TryCreateTripleTree(TripleTree *tree, char *string, int *index, TripleTree parent)
+{
+    char tempChar = string[*index];
+    *index += 1;
+    if (tempChar == '#')
+    {
+        *tree = NULL;
+        return true;
+    }
+    else
+    {
+        *tree = (TripleTreeNode *)malloc(sizeof(TripleTreeNode));
+        if (*tree == NULL)
+        {
+            printf("Create Tree Fail!");
+            return false;
+        }
+        (*tree)->data = tempChar;
+        (*tree)->lTag = 0;
+        (*tree)->rTag = 0;
+        (*tree)->flag = 0;
+        (*tree)->parent = parent;
+        TryCreateTripleTree(&(*tree)->lchild, string, index, *tree);
+        TryCreateTripleTree(&(*tree)->rchild, string, index, *tree);
     }
     return true;
 }
@@ -92,11 +132,48 @@ void PreThreadTree(Tree tree, TreeNode **pre)
     }
 }
 
+void PostThreadTree(TripleTree tree, TripleTreeNode **pre)
+{
+    if (tree != NULL)
+    {
+        PostThreadTree(tree->lchild, pre);
+        PostThreadTree(tree->rchild, pre);
+        if (!tree->lchild)
+        {
+            tree->lchild = *pre;
+            tree->lTag = 1;
+        }
+        if ((*pre) != NULL && !(*pre)->rchild)
+        {
+
+            (*pre)->rchild = tree;
+            (*pre)->rTag = 1;
+        }
+        *pre = tree;
+    }
+}
+
 TreeNode *GetFirst(TreeNode *treeNode)
 {
     while (treeNode->lTag == 0)
     {
         treeNode = treeNode->lchild;
+    }
+    return treeNode;
+}
+
+TripleTreeNode *GetFirstPostOrder(TripleTreeNode *treeNode)
+{
+    while (treeNode->lTag == 0)
+    {
+        treeNode = treeNode->lchild;
+    }
+    if (treeNode->rTag == 0 && treeNode->rchild != NULL)
+    {
+        if (GetFirstPostOrder(treeNode->rchild) != NULL)
+        {
+            treeNode = GetFirstPostOrder(treeNode->rchild);
+        }
     }
     return treeNode;
 }
@@ -124,7 +201,28 @@ TreeNode *GetNextPreOrder(TreeNode *treeNode)
         return treeNode->lchild;
     }
 }
-
+TripleTreeNode *GetNextPostOrder(TripleTreeNode *treeNode)
+{
+    if (treeNode->rTag == 1)
+    {
+        return treeNode->rchild;
+    }
+    else
+    {
+        if (treeNode->parent == NULL)
+        {
+            return NULL;
+        }
+        if (treeNode->parent->rchild == treeNode || treeNode->parent->rchild == NULL)
+        {
+            return treeNode->parent;
+        }
+        else
+        {
+            return GetFirstPostOrder(treeNode->parent->rchild);
+        }
+    }
+}
 void PrintInThreadTree(Tree tree)
 {
     TreeNode *node = GetFirst(tree);
@@ -141,6 +239,15 @@ void PrintPreThreadTree(Tree tree)
     {
         printf("%c", tree->data);
         tree = GetNextPreOrder(tree);
+    }
+}
+void PrintPostThreadTree(TripleTree tree)
+{
+    TripleTreeNode *node = GetFirstPostOrder(tree);
+    while (node != NULL)
+    {
+        printf("%c", node->data);
+        node = GetNextPostOrder(node);
     }
 }
 
@@ -161,5 +268,16 @@ int main()
     TryCreateTree(&tree, "ABD##E##CF##G##", &index);
     treeNode = NULL;
     PreThreadTree(tree, &treeNode);
+    treeNode->rchild = NULL;
+    treeNode->rTag = 1;
     PrintPreThreadTree(tree);
+    printf("\n");
+
+    TripleTree tripleTree;
+    index = 0;
+    TryCreateTripleTree(&tripleTree, "ABD##E##CF##G##", &index, tripleTree);
+    TripleTreeNode *tripleTreeNode = NULL;
+    PostThreadTree(tripleTree, &tripleTreeNode);
+    // printf(GetFirstPostOrder(tripleTree)->data);
+    PrintPostThreadTree(tripleTree);
 }
